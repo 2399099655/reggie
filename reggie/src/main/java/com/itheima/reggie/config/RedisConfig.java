7f3255ed863e4092afc7b830986e723407ba7dc3
ityuhang <2399099655@qq.com>
2023-05-14 16:29:22 +0800

    redis 相关基础配置

--- reggie/src/main/java/com/itheima/reggie/config/RedisConfig.java
+++ reggie/src/main/java/com/itheima/reggie/config/RedisConfig.java
@@ -1,16 +1,20 @@
-package src.main.java.com.itheima.reggie.config;
+package com.itheima.reggie.config;
 
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
-public class RedisConfig extends CachingConfigurerSupport {
+public class RedisConfig  extends CachingConfigurerSupport {
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory ConnectionFactory)
+    {
+        RedisTemplate<Object,Object> redisTemplate =new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setConnectionFactory(ConnectionFactory);
         return redisTemplate;
     }
-    }
+
+
+}
